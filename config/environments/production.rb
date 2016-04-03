bf83c17eae7327d077ab532e5825a907e76c0205
davehrtng <david.e.harting@gmail.com>
2016-04-03 16:31:36 -0400

    precompile assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # so that images will show up on heroku
+  config.assets.compile = true
 end
